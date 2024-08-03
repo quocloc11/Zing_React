14df0bcc53f71d6ff9f59aeb7211a0f80bc43cdc
unknown <loc842211@gmail.com>
2024-08-03 21:12:10 +0700

    setup redux

--- src/App.js
+++ src/App.js
@@ -1,52 +1,14 @@
 import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 function App() {
-  const [work, setWork] = useState('')
-  const [todos, setTodos] = useState([])
-  const handleAdd = () => {
-    if (todos?.some(item => item.id === work?.replace(/\s/g, ''))) {
-      toast.warn("Cong viec da co")
-    } else {
-      setTodos(prev => [...prev, { id: work?.replace(/\s/g, ''), job: work }])
-      setWork("")
-
-    }
-  }
-  const handleDeleteJob = (id) => {
-    setTodos(prev => prev.filter(item => item.id !== id))
-  }
+  const { test } = useSelector(state => state.app)
+  console.log(test)
   return (
     <>
-      <div className="flex flex-col gap-8 h-screen items-center border border-red-500 justify-center">
-        <div className="flex gap-8">
-          <input
-            type="text"
-            className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
-            value={work}
-            onChange={e => setWork(e.target.value)}
-          />
-          <button
-            type="button"
-            className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
-            onClick={handleAdd}
-          >
-            Add
-          </button>
-        </div>
-        <div>
-          <h3 className="font-bold text-xl">Context</h3>
-          <ul>
-            {todos?.map((item) => {
-              return (
-                <li key={item.id} className="flex gap-8 items-center">
-                  <span className="my-2">{item.job}</span>
-                  <span onClick={() => handleDeleteJob(item.id)} className="my-2 cursor-pointer">x</span>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
+      <div className="">
+        aaa
       </div>
       <ToastContainer
         position="top-right"
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import reduxConfig from './redux';
+
+const store = reduxConfig()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
